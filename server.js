dc1459e6ef90677493fcb2281f9836ae74aaecab
gettomy <sagarratangarg@gmail.com>
2018-03-18 07:04:19 +0800

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,12 +5,60 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var One_1 = {
+    title:'One_1',
+    heading:'Article One',
+    date:'March 18 2018',
+    content:`<p>
+                This is My First Step, This is My First Step, This is My First Step, This is My First Step, This is My First Step, This is My First Step, This is My First Step, This is My First Step, This is My First Step, This is My First Step, This is My First Step, This is My First Step, This is My First Step, This is My First Step, This is My First Step, This is My First Step .
+            </p>
+            <p>
+                This is My First Step, This is My First Step, This is My First Step, This is My First Step, This is My First Step, This is My First Step, This is My First Step, This is My First Step, This is My First Step, This is My First Step, This is My First Step, This is My First Step, This is My First Step, This is My First Step, This is My First Step, This is My First Step .
+            </p>
+            <p>
+                This is My First Step, This is My First Step, This is My First Step, This is My First Step, This is My First Step, This is My First Step, This is My First Step, This is My First Step, This is My First Step, This is My First Step, This is My First Step, This is My First Step, This is My First Step, This is My First Step, This is My First Step, This is My First Step .
+            </p>`
+};
+function createhtml(data){
+    var title=data.title;
+    var heading=data.heading;
+    var date=data.date;
+    var content=data.content;
+    var htmltemplate=`<html>
+        <head>
+            <title>
+                ${title}
+            </title>
+             <meta name="viewport" content="width=device-width, initial-scale=1"/>
+        <link href="/ui/style.css" rel="stylesheet" />
+        </head>
+        <body>
+            <div class="Cont">
+            <div>
+                <a href="/">Home</a>
+            </div>
+            <hr/>
+            <h3>
+               ${heading}
+            </h3>
+            <div>
+                ${date}
+            </div>
+            <div>
+                ${content}
+            </div>
+            </div>
+        </body>
+    </html>
+    `;
+    return htmltemplate;
+}
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
 app.get('/One_1', function(req,res){
-    res.sendFile(path.join(__dirname, 'ui', 'One_1.html'));
+    res.send(createhtml(One_1));
 });
 
 app.get('/One_2', function(req,res){
