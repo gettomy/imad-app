fb109c577a9103d78bc557e59131e054a283b90d
gettomy <sagarratangarg@gmail.com>
2018-03-19 05:00:42 +0800

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,7 @@
 console.log('Loaded!');
 var element=document.getElementById('main');
 element.innerHTML='HE HE';
+var img=document.getElementById('madi');
+img.onclick = function(){
+    img.style.marginLeft = '100px';
+}
