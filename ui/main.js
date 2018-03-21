25fd9e3e8276b1c7081e9273fabf2ff33b4fb0f5
gettomy <sagarratangarg@gmail.com>
2018-03-22 03:28:12 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -11,7 +11,6 @@ button.onclick = function (){
           }
       }  
     };
-    counter=counter + 1;
-    var span=document.getElementById('count');
-    span.innerHTML=counter.toString();
+    response.open('GET','http://sagarratangarg.imad.hasura-app.io/counter',true);
+    response.send(null);
 };
