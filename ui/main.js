cb77297d02b637b2923bd5293612036f18d20db4
gettomy <sagarratangarg@gmail.com>
2018-03-22 03:41:50 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,8 +4,10 @@ button.onclick = function (){
     var request= new XMLHttpRequest();
     
     request.onreadystatechange = function (){
-      if(request.readyState===XMLHttpRequest.DONE){
+      if(request.readyState === XMLHttpRequest.DONE){
+          
           if(request.status===200){
+              
               var counter = request.responseText;
               var span=document.getElementById('count');
               span.innerHTML=counter.toString();
