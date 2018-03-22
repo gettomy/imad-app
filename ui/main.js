6d3270a9cbf68c896cc582919d41b66e1bf8abca
gettomy <sagarratangarg@gmail.com>
2018-03-23 02:19:08 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -22,11 +22,23 @@ var nameInput = document.getElementById('name');
 var name = nameInput.value;
 var submit = document.getElementById('submit-btn');
 submit.onclick = function(){
-    var names = ['name1','name2','name3','name4'];
-    var list='';
-    for (var i=0;i<names.length;i++){
-        list+='<li>' + names[i] + '</li>';
-}
+      var request= new XMLHttpRequest();
+    
+    request.onreadystatechange = function (){
+      if(request.readyState === XMLHttpRequest.DONE){
+          
+          if(request.status===200){
+              
+               // var names = ['name1','name2','name3','name4'];
+                var list='';
+                for (var i=0;i<names.length;i++){
+                    list+='<li>' + name + '</li>';
+          }
+      }  
+    }
+    request.open('GET','http://sagarratangarg.imad.hasura-app.io/submit-name?name='+name,true);
+    request.send(null);
+};
 var ul = document.getElementById('namelist');
 ul.innerHTML = list;
 };
