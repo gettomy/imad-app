573e7dfe40cf32ee40fff292d8e4b3134f68d997
gettomy <sagarratangarg@gmail.com>
2018-03-23 02:21:52 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -31,9 +31,9 @@ submit.onclick = function(){
               
                // var names = ['name1','name2','name3','name4'];
                 var list='';
-                for (var i=0;i<names.length;i++){
+                //for (var i=0;i<names.length;i++){
                     list+='<li>' + name + '</li>';
-                }
+                //}
                 var ul = document.getElementById('namelist');
                 ul.innerHTML = list;
 
